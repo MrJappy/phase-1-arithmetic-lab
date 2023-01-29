6d7b0b4e576a2ecad8f4d61ac97570b5b3ae83de
+ <+>
2023-01-28 21:23:36 -0500

    adding JS

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+let random;
+random = Math.round(Math.random() * 10) + 1;
+
+
+let num3 = 22;
+let num4 = 18;
+const mod = num3 % num4;
+console.log(mod)
+
+const max = Math.max(1,2,3,4,20)
